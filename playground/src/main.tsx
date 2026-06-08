97d564e271aaada0c0cd64504a77de72611bda8b
RITZ_ <ritz1014.ritz1014@gmail.com>
2026-06-08 23:15:07 +0900

    feat(playground): change style

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -5,7 +5,7 @@ import { PixelCanvas, Button } from "@pixeln/renderer";
 function App() {
   return (
     <div>
-      <h1 style={{ color: "#fff", fontFamily: "monospace" }}>Pixeln Playground</h1>
+      <h1 style={{ color: "#000000", fontFamily: "monospace" }}>Pixeln Playground</h1>
       <PixelCanvas width={32} height={32} scale={12} grid>
         <box x={0} y={0} w={32} h={32} bg="#350025" />
         <Button x={4} y={12} w={12} h={6} bg="blue" onClick={() => alert("Clicked!")} />
